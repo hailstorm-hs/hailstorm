344183f8962bb24757b26d3929c4b28813d06705
Thomas Dimson <tdimson@fb.com>
2014-05-11 16:15:59 -0700

    Wiring up pipes for the spout

--- src/Hailstorm/Processors.hs
+++ src/Hailstorm/Processors.hs
@@ -1,10 +1,31 @@
 module Hailstorm.Processors where
 
+import Control.Monad
 import Hailstorm.UserFormula
+import Pipes
+import System.IO
 import qualified Data.ByteString as B
 
-runSpout :: (Show k, Show v) => (UserFormula k v) -> IO ()
-runSpout uf = do
-    let (k,v) = convertFn uf $ B.empty
-    print k
-    print v
+data DataPartitionOffset = DataPartitionOffset B.ByteString String Integer
+    deriving (Eq, Show, Read)
+
+data TuplePartitionOffset k v = TuplePartitionOffset (k,v) String Integer
+    deriving (Eq, Show, Read)
+
+tupleSpoutProducer :: (Show k, Show v, Monad m) => UserFormula k v -> Producer DataPartitionOffset m () -> Producer (TuplePartitionOffset k v) m ()
+tupleSpoutProducer uf producer = for producer (\x -> case x of (DataPartitionOffset bs p o) ->  yield (TuplePartitionOffset (convertFn uf bs) p o))
+
+
+handleLineProducer :: Handle -> String -> Integer -> Producer DataPartitionOffset IO ()
+handleLineProducer h partitionName offset = do
+    eof <- lift $ hIsEOF h
+    when eof $ lift (hSeek h AbsoluteSeek 0)
+    l <- lift $ B.hGetLine h
+    yield $ DataPartitionOffset l partitionName offset
+    handleLineProducer h partitionName (offset + 1)
+
+
+fileLineProducer :: FilePath -> Producer DataPartitionOffset IO ()
+fileLineProducer fp = do
+    h <- lift $ openFile fp ReadMode
+    handleLineProducer h (show fp) 0
--- src/HailstormSample.hs
+++ src/HailstormSample.hs
@@ -3,13 +3,19 @@ module Main where
 import Data.Monoid
 import Hailstorm.Processors
 import Hailstorm.UserFormula
+import Pipes
+
+import qualified Data.ByteString.Char8 as C8
 
 adderFormula :: UserFormula String (Sum Int)
 adderFormula = UserFormula {
-    convertFn = \x -> ("bieber", Sum 1)
+    convertFn = \x -> (C8.unpack x, Sum 1)
   , outputFn = \_ -> print "hi"
 }
 
+main :: IO ()
 main = do
     print "Hello world"
-    runSpout adderFormula
+    let f = fileLineProducer "data/test.txt"
+    let t = tupleSpoutProducer adderFormula f
+    runEffect $ for t (lift . print)
