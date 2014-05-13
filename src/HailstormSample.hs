edfd406348d334683b47750da922fa16c5da9540
Thomas Dimson <tdimson@fb.com>
2014-05-12 23:56:47 -0700

    Example of piping pipes through a socket

--- src/HailstormSample.hs
+++ src/HailstormSample.hs
@@ -1,11 +1,17 @@
 module Main where
 
+import Control.Concurrent
 import Data.Monoid
 import Hailstorm.Processors
 import Hailstorm.UserFormula
 import Pipes
+import Pipes.Network.TCP
 
 import qualified Data.ByteString.Char8 as C8
+import qualified Pipes.Prelude as P
+
+consumerPort :: String
+consumerPort = "8000"
 
 adderFormula :: UserFormula String (Sum Int)
 adderFormula = UserFormula {
@@ -13,9 +19,22 @@ adderFormula = UserFormula {
   , outputFn = \_ -> print "hi"
 }
 
+consumerLoop :: (Socket, SockAddr) -> IO ()
+consumerLoop (s, sa) = do
+    putStrLn $ "Switching consumer loop on " ++ show sa
+    runEffect $ fromSocket s 4096 >-> P.map (\x -> C8.unpack x ++ "\n") >-> P.stdoutLn
+
+consumerAcceptor :: IO ()
+consumerAcceptor = serve HostAny consumerPort consumerLoop
+
 main :: IO ()
 main = do
-    print "Hello world"
+    consumerId <- forkIO consumerAcceptor
+    putStrLn $ "Spawned consumer thread " ++ show consumerId
+    threadDelay 1000000
     let f = fileLineProducer "data/test.txt"
     let t = tupleSpoutProducer adderFormula f
-    runEffect $ for t (lift . print)
+
+    connect "127.0.0.1" consumerPort $ \(s,_) -> do
+        let c = toSocket s
+        runEffect $ t >-> P.map (C8.pack . show) >-> c
