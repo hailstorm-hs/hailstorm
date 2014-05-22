de5d5858f4f333b85bff87ba4119af5f6d407151
Thomas Dimson <tdimson@fb.com>
2014-05-22 00:48:08 -0700

    Monitor master state function

--- src/Hailstorm/ZKCluster.hs
+++ src/Hailstorm/ZKCluster.hs
@@ -5,6 +5,7 @@ module Hailstorm.ZKCluster
 , createMasterState
 , getStatus
 , initializeCluster
+, monitorMasterState
 , quietZK
 , registerProcessor
 , setMasterState
@@ -62,22 +63,39 @@ getStatus opts = withConnection opts $ \zk -> do
         Left e  -> return $ "Error from zookeeper: " ++ show e
         Right p -> return $ "Living processors: " ++ show p
 
+-- | Delivers children change events to the callback. Uses the same thread
+-- as was called in with
 childrenWatchLoop :: ZK.Zookeeper -> String -> ([String] -> IO ()) -> IO ()
 childrenWatchLoop zk path cb = do
     childrenVar <- newMVar True
     _ <- ZK.getChildren zk path (Just $ watcher childrenVar)
     childLoop childrenVar
 
-    where watcher childrenVar _ _ _ _ = do
-            putStrLn $ "Watcher saw a node change"
-            putMVar childrenVar True
-
+    where watcher childrenVar _ _ _ _ = putMVar childrenVar True
           childLoop childrenVar = forever $ do
             _ <- takeMVar childrenVar
             me <- ZK.getChildren zk path (Just $ watcher childrenVar)
             case me of
               Left e -> putStrLn $ "Error in children watch loop from zookeeper: " ++ (show e)
               Right children -> cb children
+
+-- | Delivers master state change events to the callback. Uses the same thread
+-- as was called in with
+monitorMasterState :: ZK.Zookeeper -> (Either ZK.ZKError MasterState -> IO ()) -> IO ()
+monitorMasterState zk cb = do
+    mVar <- newMVar True
+    _ <- ZK.get zk zkMasterStateNode (Just $ watcher mVar)
+    monitorLoop mVar
+
+    where watcher mVar _ _ _ _ = putMVar mVar True
+          monitorLoop mVar = forever $ do
+            _ <- takeMVar mVar
+            me <- ZK.get zk zkMasterStateNode Nothing
+
+            case me of 
+                (Left e) -> cb $ Left e
+                (Right ((Just s), _)) -> cb $ Right $ deserializeMasterState s
+                _ -> cb $ Left $ ZK.NothingError
 
 deserializeMasterState :: BS.ByteString -> MasterState
 deserializeMasterState = read . C8.unpack
