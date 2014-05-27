c568945762ce87414eb682ad37d4994ac6894470
Milind Ganjoo <mganjoo@stanford.edu>
2014-05-26 23:06:09 -0700

    Add correct low water mark recalculation

--- src/Hailstorm/Clock.hs
+++ src/Hailstorm/Clock.hs
@@ -2,6 +2,7 @@ module Hailstorm.Clock
 ( Clock(..)
 , Partition
 , Offset
+, extractClockMap
 ) where
 
 import qualified Data.Map as Map
@@ -10,4 +11,7 @@ type Partition = String
 type Offset = Integer
 
 newtype Clock = Clock (Map.Map Partition Offset)
-                deriving (Eq, Show, Read)
+                deriving (Eq, Show, Read, Ord)
+
+extractClockMap :: Clock -> Map.Map Partition Offset
+extractClockMap (Clock clk) = clk
--- src/Hailstorm/Processor/Downstream.hs
+++ src/Hailstorm/Processor/Downstream.hs
@@ -40,7 +40,7 @@ runDownstream opts dId@(dName, _) topology uformula = do
         producer = socketProducer uformula
         consumer mStateMVar =
             case ctype of
-                Bolt -> boltPipe uformula mStateMVar Map.empty Map.empty >->
+                Bolt -> boltPipe dName uformula mStateMVar Map.empty Map.empty >->
                     downstreamPoolConsumer dName topology uformula
                 Sink -> sinkConsumer uformula
                 _ -> throw $ InvalidTopologyError $
@@ -72,55 +72,71 @@ socketProducer uformula s = do
 -- | Builds a Pipe that receives a payload emitted from a handle and
 -- performs the monoidal append operation associated with the given processor.
 boltPipe :: (Ord k, Monoid v)
-         => UserFormula k v
+         => ProcessorName
+         -> UserFormula k v
          -> MVar MasterState
          -> BoltState k v
          -> BoltState k v
          -> Pipe (Payload k v) (Payload k v) IO ()
-boltPipe uformula mStateMVar preSnapshotState postSnapshotState = do
+boltPipe bName uformula mStateMVar preSnapshotState postSnapshotState = do
     payload <- await
 
     let (key, val) = payloadTuple payload
         (partition, offset) = payloadPosition payload
-        calcLowWaterMarkMap = id -- TODO: calculate
-        mergeWithVal st = Map.unionWith mappend st $ Map.singleton key val
-        mergeStates = Map.unionWith mappend
-        passOnWithSnapshot stateA stateB = do
+        lwmMap = payloadLowWaterMarkMap payload
+        newLWM = buildLWM lwmMap
+        newLWMMap = Map.union (Map.singleton bName newLWM) lwmMap
+
+    let passOn stateA stateB nextSnapshotClock = do
             let valA = Map.findWithDefault mempty key stateA
                 valB = Map.findWithDefault mempty key stateB
+                canSnapshot (Just clk) = newLWM `isNewerClock` clk
+                canSnapshot Nothing = False
+
             yield Payload { payloadTuple = (key, valA `mappend` valB)
-                          , payloadPosition = payloadPosition payload
-                          , payloadLowWaterMarkMap = calcLowWaterMarkMap $
-                              payloadLowWaterMarkMap payload
+                          , payloadPosition = (partition, offset)
+                          , payloadLowWaterMarkMap = newLWMMap
                           }
-            -- TODO: save snapshot.
-            boltPipe uformula mStateMVar stateA stateB
-        passOnNoSnapshot st = do
-            let val' = Map.findWithDefault mempty key st
-            yield Payload { payloadTuple = (key, val')
-                          , payloadPosition = payloadPosition payload
-                          , payloadLowWaterMarkMap = calcLowWaterMarkMap $
-                              payloadLowWaterMarkMap payload
-                          }
-            boltPipe uformula mStateMVar st Map.empty
+
+            if canSnapshot nextSnapshotClock
+                then do
+                    lift $ saveSnapshot stateA $ fromJust nextSnapshotClock
+                    boltPipe bName uformula mStateMVar
+                        (stateA `mergeStates` stateB) Map.empty
+                else boltPipe bName uformula mStateMVar stateA stateB
 
     -- Determine next snapshot clock, if available.
     mState <- lift $ readMVar mStateMVar
-    case mState of
-        Flowing mClock ->
-            case mClock of
-                Just (Clock nextSnapshotMap) -> do
-                    let desiredOffset = nextSnapshotMap Map.! partition
-                    if offset > desiredOffset
-                        then passOnWithSnapshot preSnapshotState
-                            (mergeWithVal postSnapshotState)
-                        else passOnWithSnapshot (mergeWithVal preSnapshotState)
-                            postSnapshotState
-                Nothing -> passOnNoSnapshot $ mergeWithVal
-                    (preSnapshotState `mergeStates` postSnapshotState)
+    case getNextSnapshotClock mState of
+        Just desiredClock@(Clock clockMap) -> do
+            let desiredOffset = clockMap Map.! partition
+            if offset > desiredOffset
+                then let b' = mergeWithTuple postSnapshotState (key, val)
+                     in passOn preSnapshotState b' (Just desiredClock)
+                else let a' = mergeWithTuple preSnapshotState (key, val)
+                     in passOn a' postSnapshotState (Just desiredClock)
+        Nothing ->
+            let mergedState = preSnapshotState `mergeStates` postSnapshotState
+            in passOn (mergeWithTuple mergedState (key, val)) Map.empty Nothing
+  where
+    getNextSnapshotClock (Flowing (Just clk)) = Just clk
+    getNextSnapshotClock _ = Nothing
+    mergeWithTuple st (key, val) = Map.unionWith mappend st $
+        Map.singleton key val
+    mergeLWM = Map.unionWith min
+    mergeStates = Map.unionWith mappend
+    buildLWM lwmMap = Clock $ foldr (mergeLWM . extractClockMap) Map.empty $
+        Map.elems lwmMap
+    isNewerClock (Clock clk1) (Clock clk2) =
+        let (x:xs) `gtElems` (y:ys) = x > y && xs `gtElems` ys
+            [] `gtElems` [] = True
+            _ `gtElems` _ = False
+        in (Map.keys clk1 == Map.keys clk2) &&
+            (Map.elems clk1 `gtElems` Map.elems clk2)
 
-        _ -> passOnNoSnapshot $ mergeWithVal
-            (preSnapshotState `mergeStates` postSnapshotState)
+-- TODO: fork new OS thread and save to disk
+saveSnapshot :: BoltState k v -> Clock -> IO ()
+saveSnapshot _ _ = return ()
 
 -- | Builds a Consumer that receives a payload emitted from a handle and
 -- performs the sink operation defined in the given user formula.
