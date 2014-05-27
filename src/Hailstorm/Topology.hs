69bc6bb11eb16d9b5a726362e455e740e9a58340
Milind Ganjoo <mganjoo@stanford.edu>
2014-05-26 19:39:39 -0700

    Add 'upstreamProcessorNames' functoin

--- src/Hailstorm/Topology.hs
+++ src/Hailstorm/Topology.hs
@@ -10,17 +10,25 @@ import Hailstorm.Processor
 import qualified Data.Map as Map
 
 class Topology t where
+
     processors :: t
                -> Map.Map String Processor
+
     downstreamAddresses :: t
                         -> ProcessorName
                         -> Payload k v
                         -> [ProcessorAddress]
+
     addressFor :: t
                -> ProcessorId
                -> ProcessorAddress
+
     numProcessors :: t
                   -> Int
+
+    upstreamProcessorNames :: t
+                           -> ProcessorName
+                           -> [ProcessorName]
 
 type ProcessorHost = String
 type ProcessorPort = String
@@ -32,7 +40,9 @@ data HardcodedTopology = HardcodedTopology
     } deriving (Eq, Show, Read)
 
 instance Topology HardcodedTopology where
+
     downstreamAddresses t processorName _ =
+        -- TODO: hash tuple value to determine which instance to deliver to
         let upstream = fromJust $ Map.lookup processorName (processorMap t)
             findAddress downstreamName =
                 let downstream = fromJust $
@@ -40,10 +50,22 @@ instance Topology HardcodedTopology where
                 in fromJust $ Map.lookup
                     (name downstream, parallelism downstream - 1) (addresses t)
         in map findAddress (downstreams upstream)
+
     addressFor t (processorName, processorNumber) = fromJust $
         Map.lookup (processorName, processorNumber) (addresses t)
-    numProcessors (HardcodedTopology pmap _) = Map.fold (\p l -> l + parallelism p) 0 pmap
+
+    numProcessors (HardcodedTopology pmap _) =
+        Map.fold (\p l -> l + parallelism p) 0 pmap
+
     processors = processorMap
 
+    upstreamProcessorNames t pName =
+        let lookupInDownstreams p = if pName `elem` downstreams p
+                                        then Just (name p)
+                                        else Nothing
+        in mapMaybe lookupInDownstreams $ Map.elems (processorMap t)
+
 spoutIds :: (Topology t) => t -> [ProcessorId]
-spoutIds t = [(n,c) | (_, Processor Spout n p _) <- Map.toList (processors t), c <- [0..(p-1)]]
+spoutIds t = [(n,c)
+             | (_, Processor Spout n p _) <- Map.toList (processors t)
+             , c <- [0..(p - 1)]]
