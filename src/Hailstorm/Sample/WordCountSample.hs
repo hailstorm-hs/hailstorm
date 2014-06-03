f8c25f61eeb853bca83c30dbbbb6e675be10b560
Thomas Dimson <tdimson@gmail.com>
2014-06-02 23:34:55 -0700

    Sorter bolt layer, not quite working

--- src/Hailstorm/Sample/WordCountSample.hs
+++ src/Hailstorm/Sample/WordCountSample.hs
@@ -6,8 +6,10 @@ module Hailstorm.Sample.WordCountSample
 
 import Control.Exception
 import Data.Dynamic
+import Data.List
 import Data.Maybe
 import Data.Monoid
+import Data.Ord
 import Hailstorm.Error
 import Hailstorm.Topology.HardcodedTopology
 import Hailstorm.Processor
@@ -19,7 +21,10 @@ localServer :: String
 localServer = "127.0.0.1"
 
 data TypeableIntSum = TIS (Sum Int)
-                      deriving (Typeable, Show, Read)
+                      deriving (Eq, Typeable, Show, Read)
+
+instance Ord TypeableIntSum where
+   (TIS (Sum a)) `compare` (TIS (Sum b)) = a `compare` b
 
 instance Monoid TypeableIntSum where
     mempty = TIS (mempty :: (Sum Int))
@@ -37,6 +42,10 @@ dynToWordCountTuple :: Dynamic -> (String, TypeableIntSum)
 dynToWordCountTuple d = flip fromMaybe (fromDynamic d) $
     throw $ InvalidTopologyError "Unexpected value: not a (word, count) tuple"
 
+dynToListTuple :: Dynamic -> [(String, TypeableIntSum)]
+dynToListTuple d = flip fromMaybe (fromDynamic d) $
+    throw $ InvalidTopologyError "Unexpected value: not a [(word, count)] tuple"
+
 dynToMMWrapper :: Dynamic -> MonoidMapWrapper String TypeableIntSum
 dynToMMWrapper d = flip fromMaybe (fromDynamic d) $
     throw $ InvalidTopologyError "Unexpected value: not a word-count) state map"
@@ -48,8 +57,14 @@ tupleToState tup = let (key, val) = payloadTupleToWordCountTuple tup
 payloadTupleToWordCountTuple :: PayloadTuple -> (String, TypeableIntSum)
 payloadTupleToWordCountTuple (MkPayloadTuple d) = dynToWordCountTuple d
 
-readPayloadTuple :: String -> PayloadTuple
-readPayloadTuple x = MkPayloadTuple $ toDyn (read x :: (String, TypeableIntSum))
+payloadTupleToListTuple :: PayloadTuple -> [(String, TypeableIntSum)]
+payloadTupleToListTuple (MkPayloadTuple d) = dynToListTuple d
+
+readTISPayloadTuple :: String -> PayloadTuple
+readTISPayloadTuple x = MkPayloadTuple $ toDyn (read x :: (String, TypeableIntSum))
+
+readListPayloadTuple :: String -> PayloadTuple
+readListPayloadTuple x = MkPayloadTuple $ toDyn (read x :: [(String, TypeableIntSum)])
 
 boltStateToInnerMap :: BoltState -> Map.Map String TypeableIntSum
 boltStateToInnerMap (MkBoltState d) = let (MonoidMapWrapper m') = dynToMMWrapper d
@@ -74,6 +89,12 @@ lookupTupleInState tup (MkBoltState dynM) =
             (error $ "Could not find " ++ k ++ " in state " ++ show m) k m
     in (MkPayloadTuple $ toDyn (k, v))
 
+outputBoltState :: PayloadTuple -> BoltState -> PayloadTuple
+outputBoltState _ (MkBoltState dynM) = 
+    let MonoidMapWrapper m = dynToMMWrapper dynM
+        sorted = sortBy (comparing $ snd) (Map.toList m)
+    in (MkPayloadTuple $ toDyn sorted)
+
 wordsSpout :: Spout
 wordsSpout = Spout
     { spoutName = "words"
@@ -86,7 +107,7 @@ countBolt :: Bolt
 countBolt = Bolt
     { boltName = "count"
     , boltParallelism = 1
-    , upstreamDeserializer = readPayloadTuple
+    , upstreamDeserializer = readTISPayloadTuple
     , downstreamSerializer = show . payloadTupleToWordCountTuple
     , stateDeserializer = readBoltState
     , stateSerializer = show . boltStateToInnerMap
@@ -96,12 +117,27 @@ countBolt = Bolt
     , transformTupleFn = lookupTupleInState
     }
 
+sortBolt :: Bolt
+sortBolt = Bolt
+    { boltName = "sort"
+    , boltParallelism = 1
+    , upstreamDeserializer = readTISPayloadTuple
+    , downstreamSerializer = show . payloadTupleToListTuple
+    , stateDeserializer = readBoltState
+    , stateSerializer = show . boltStateToInnerMap
+    , tupleToStateConverter = tupleToState
+    , emptyState = MkBoltState $ toDyn (mempty :: MonoidMapWrapper String TypeableIntSum)
+    , mergeFn = mergeStates
+    , transformTupleFn = outputBoltState
+    }
+
 outputSink :: Sink
 outputSink = Sink
     { sinkName = "sink"
     , sinkParallelism = 1
-    , outputFn = \_ -> return ()
-    , sinkDeserializer = readPayloadTuple
+    --, outputFn = \_ -> return ()
+    , outputFn = \x -> print (payloadTupleToListTuple x) 
+    , sinkDeserializer = readListPayloadTuple
     }
 
 wordCountTopology :: HardcodedTopology
@@ -110,16 +146,19 @@ wordCountTopology = HardcodedTopology
       processorNodeMap = mkProcessorMap
       [ SpoutNode wordsSpout
       , BoltNode countBolt
+      , BoltNode sortBolt
       , SinkNode outputSink
       ]
       ,
       downstreamMap = Map.fromList
       [ ("words", [("count", const 0)]) -- TODO: implement grouping function
-      , ("count", [("sink", const 0)]) -- TODO: implement grouping function
+      , ("count", [("sort", const 0)]) -- TODO: implement grouping function
+      , ("sort", [("sink", const 0)]) -- TODO: implement grouping function
       ]
       ,
       addresses = Map.fromList
       [ (("sink", 0), (localServer, "10000"))
       , (("count", 0), (localServer, "10001"))
+      , (("sort", 0), (localServer, "10002"))
       ]
   }
